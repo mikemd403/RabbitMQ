f47eca1ec97f0a0ae05115a3d77133af518160c6
Stéphane Somé <stephanesome@gmail.com>
2020-10-10 01:29:04 -0400

    Final version

--- src/main/kotlin/seg3102/rabbitchat/RabbitMQConfiguration.kt
+++ src/main/kotlin/seg3102/rabbitchat/RabbitMQConfiguration.kt
@@ -1,4 +1,25 @@
 package seg3102.rabbitchat
 
+import org.springframework.amqp.core.*
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
 class RabbitMQConfiguration {
+    @Bean
+    fun queue(): AnonymousQueue {
+        return AnonymousQueue()
+    }
+
+    @Bean
+    fun exchange(): FanoutExchange {
+        return FanoutExchange("chatfan")
+    }
+
+    @Bean
+    fun binding(exchange: FanoutExchange,
+                queue: Queue): Binding {
+        return BindingBuilder.bind(queue)
+                .to(exchange)
+    }
 }
